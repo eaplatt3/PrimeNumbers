2530bea85a5f92ad218ea0bd55bb32b308ef0409
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-03-06 11:38:09 -0500

    Update PrimeNumbers.pl
    Added Commets and Cleaned up code

--- PrimeNumbers.pl
+++ PrimeNumbers.pl
@@ -1,13 +1,24 @@
+####################################
+# Earl Platt III
+# 3/6/2019
+# BCS 316 - Perl Programming
+# Prime Number Sort
+# Input - Static Numbers 1 - 1000
+# Output - Display Sorted Prime
+#          Numbers from 1 - 1000
+####################################
+
 use strict;
 use warnings; 
 
-# Variables 
+# Variable 
 my $nums = 0;
 
 print "\n";
 print "The Prime Numbers From 1-1000";
 print "\n";
 
+#Loop to sort prime numbers
 while($nums != 1000){
 	$nums++;
 	if($nums % 2 == 0){
@@ -15,4 +26,5 @@ while($nums != 1000){
 		print "\n";
 	}
 }
+
 print "\n";
